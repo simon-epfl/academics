ee53a78190c5de6cccb18a8a224068d05721be86
Androz 2091 <androz2091@gmail.com>
2025-01-31 19:03:39 +0100

    add some content

--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,349 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { Badge } from "@/components/ui/badge"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 import Image from "next/image";
+
+const codes = {
+  'CS': 'Computer Science',
+  'MA': 'Mathematics',
+  'COM': 'Communication Systems',
+  'PHYS': 'Physics',
+  'default': 'Other'
+}
+
+const colorMapping = {
+  'CS': '#FBE2B1',
+  'MA': '#D3EEE6',
+  'COM': '#D3EEE6',
+  'PHYS': '#D2D9EE',
+  'default': '#F9F9ED'
+}
+
+const semesters = [
+  {
+    title: 'Bachelor Semester 3',
+    courses: [
+      {
+        title: "Software Construction",
+        code: "CS-214",
+        url: "https://cs-214.epfl.ch/",
+        buttons: [{
+          title: "Lecture notes I",
+          url: "https://github.com/simon-epfl/notes-ba3-simon/blob/main/softcon/midterm.pdf"
+        },
+        {
+          title: "Lecture notes II",
+          url: "https://github.com/simon-epfl/notes-ba3-simon/blob/main/softcon/final.pdf"
+        },
+        {
+          title: "Exercises (solutions)",
+          url: "https://github.com/simon-epfl/notes-ba3-simon/blob/main/softcon/exercises"
+        }]
+      },
+      {
+        title: "Probability and Statistics",
+        code: "MA-232",
+        url: "https://edu.epfl.ch/coursebook/fr/probability-and-statistics-for-ic-MATH-232",
+        buttons: [{
+          title: "Lecture notes",
+          url: "https://github.com/simon-epfl/notes-ba3-simon/blob/main/probastats/Probastats_Simon_Notes_Tricks.pdf"
+        }]
+      },
+      {
+        title: "Analysis III",
+        code: "MA-203",
+        url: "https://cs-214.epfl.ch/",
+        buttons: [{
+          title: "Lecture notes",
+          url: "https://github.com/simon-epfl/notes-ba3-simon/blob/main/analyseiii/Notes_Analyse_III_Simon.pdf"
+        },
+        {
+          title: "Cheat Sheet (final)",
+          url: "https://github.com/simon-epfl/notes-ba3-simon/blob/main/analyseiii/Formulaire_Analyse_III_Simon.pdf"
+        }]
+      },
+      {
+        title: "Electromagnetism",
+        code: "PHYS-114",
+        url: "https://edu.epfl.ch/coursebook/fr/physique-generale-electromagnetisme-PHYS-114",
+        buttons: [{
+          title: "Cheat Sheet (final)",
+          url: "https://github.com/simon-epfl/notes-ba3-simon/blob/main/electromag/Formulaire_Final_Electromagnetisme_Simon.pdf"
+        },
+        {
+          title: "Exercises (solutions)",
+          url: "https://github.com/simon-epfl/notes-ba3-simon/blob/main/electromag/Tricks_Series_Electromagnetisme_Simon.pdf"
+        }]
+      },
+      {
+        title: "Computer Architecture",
+        code: "CS-200",
+        url: "https://cs-214.epfl.ch/",
+        buttons: [{
+          title: "Lecture notes I",
+          url: "https://github.com/simon-epfl/notes-ba3-simon/blob/main/comparch/Notes_Comparch_Midterm_Simon.pdf"
+        },
+        {
+          title: "Notes Pipelining",
+          url: "https://github.com/simon-epfl/notes-ba3-simon/blob/main/comparch/Notes_Comparch_Midterm_Simon.pdf"
+        },
+        {
+          title: "Notes Multiprocessors",
+          url: "https://github.com/simon-epfl/notes-ba3-simon/blob/main/comparch/Notes_Comparch_Multiprocessors_Simon.pdf"
+        },
+        {
+          title: "Exercises (solutions)",
+          url: "https://github.com/simon-epfl/notes-ba3-simon/blob/main/comparch/Notes_Comparch_Exercices_Simon.pdf"
+        }]
+      },
+      {
+        title: "State and human rights",
+        code: "HUM-208",
+        url: "https://edu.epfl.ch/coursebook/en/state-and-human-rights-HUM-208",
+        buttons: [{
+          title: "Lecture Notes",
+          url: "https://github.com/simon-epfl/notes-ba3-simon/blob/main/shs/notes.pdf"
+        }]
+      }
+    ]
+  },
+  {
+    title: 'Bachelor Semester 2',
+    courses: [
+      {
+        title: "Advanced ICC II",
+        shiny: true,
+        code: "COM-102",
+        url: "https://edu.epfl.ch/coursebook/en/advanced-information-computation-communication-ii-COM-102",
+        buttons: [{
+          title: "Lecture notes",
+          url: "https://github.com/simon-epfl/notes-ba2-simon/blob/main/aicc/cours.pdf"
+        }, {
+          title: "Exercises (solutions)",
+          url: "https://github.com/simon-epfl/notes-ba2-simon/tree/main/aicc/series"
+        }]
+      },
+      {
+        title: "Fundamentals of Digital Systems",
+        code: "CS-173",
+        url: "https://edu.epfl.ch/coursebook/fr/fundamentals-of-digital-systems-CS-173",
+        shiny: true,
+        buttons: [{
+          title: "Notes de cours I",
+          url: "https://github.com/simon-epfl/notes-ba2-simon/blob/main/fds/fds-tricks-1.pdf"
+        },
+        {
+          title: "Notes de cours II",
+          url: "https://github.com/simon-epfl/notes-ba2-simon/blob/main/fds/fds-tricks-2.pdf"
+        },
+        {
+          title: "Verilog",
+          url: "https://github.com/simon-epfl/notes-ba2-simon/blob/main/fds/guide_verilog.pdf"
+        },
+        {
+          title: "Single/Multi-cycle CPU",
+          url: "https://github.com/simon-epfl/notes-ba2-simon/blob/main/fds/multicycle.pdf"
+        },
+        {
+          title: "Logisim Circuits",
+          url: "https://github.com/simon-epfl/notes-ba2-simon/blob/main/fds/logisim"
+        },
+        {
+          title: "Verilog Programs",
+          url: "https://github.com/simon-epfl/notes-ba2-simon/blob/main/fds/verilog"
+        },
+        {
+          title: "RISC-V Programs",
+          url: "https://github.com/simon-epfl/notes-ba2-simon/blob/main/fds/risc-v"
+        },
+        {
+          title: "Exercises",
+          url: "https://github.com/simon-epfl/notes-ba2-simon/tree/main/fds"
+        },
+        {
+          title: "ETH exams",
+          url: "https://github.com/simon-epfl/notes-ba2-simon/blob/main/fds/eth-exams.pdf"
+        }]
+      },
+      {
+        title: "Analysis II",
+        code: "MA-106",
+        url: "https://edu.epfl.ch/coursebook/fr/analyse-ii-MATH-106-E",
+        buttons: [{
+          title: "Lecture notes",
+          url: "https://github.com/simon-epfl/notes-ba2-simon/blob/main/analyse/analyse-tricks.pdf"
+        },
+        {
+          title: "Démonstrations",
+          url: "https://github.com/simon-epfl/notes-ba2-simon/blob/main/analyse/demos.pdf"
+        },
+        {
+          title: "Séries rédigées",
+          url: "https://github.com/simon-epfl/notes-ba2-simon/tree/main/analyse/series"
+        }]
+      },
+      {
+        title: "Object-Oriented Programming",
+        code: "CS-108",
+        url: "https://cs108.epfl.ch/",
+        buttons: [{
+          title: "Formulaire examen final",
+          url: "https://github.com/simon-epfl/notes-ba2-simon/blob/main/prog/formulaire.pdf"
+        }]
+      },
+      {
+        title: "SHS Communication B",
+        code: "HUM-122",
+        url: "https://graphsearch.epfl.ch/fr/course/HUM-122(b)",
+        buttons: [{
+          title: "Lecture Notes",
+          url: "https://github.com/simon-epfl/notes-ba2-simon/blob/main/shs/cours.pdf"
+        }, {
+          title: "Poster (final)",
+          url: "https://github.com/simon-epfl/notes-ba2-simon/blob/main/shs/poster.pdf"
+        }]
+      }
+    ]
+  },
+  {
+    title: 'Bachelor Semester 1',
+    courses: [
+      {
+        title: "General physics: mechanics",
+        code: "PHYS-101",
+        url: "https://edu.epfl.ch/coursebook/fr/physique-generale-mecanique-PHYS-101-F",
+        buttons: [{
+          title: "Formulaire",
+          url: "https://github.com/simon-epfl/notes-ba1-simon/blob/master/mecanique/Me%CC%81canique%20Cheat%20Sheet.pdf"
+        }]
+      },
+      {
+        title: "Advanced ICC I",
+        code: "CS-101",
+        url: "https://edu.epfl.ch/coursebook/fr/advanced-information-computation-communication-i-CS-101",
+        buttons: [{
+          title: "Cheat Sheet (counting)",
+          url: "https://github.com/simon-epfl/notes-ba1-simon/blob/master/aicc/AICC%20Counting%20Sheet.pdf"
+        },
+        {
+          title: "Exercises (solutions)",
+          url: "https://github.com/simon-epfl/notes-ba1-simon/tree/master/aicc"
+        }]
+      },
+      {
+        title: "Linear algebra",
+        code: "MA-111",
+        url: "https://edu.epfl.ch/coursebook/fr/algebre-lineaire-MATH-111-E",
+        buttons: [{
+          title: "Quizlet (final)",
+          url: "https://github.com/simon-epfl/notes-ba1-simon/blob/master/algebre/Quizlet%20Alg%C3%A8bre.pdf"
+        }]
+      },
+      {
+        title: "Analysis I",
+        code: "MA-101",
+        url: "https://edu.epfl.ch/coursebook/fr/analyse-i-MATH-101-E",
+        buttons: [{
+          title: "Quizlet (final)",
+          url: "https://github.com/simon-epfl/notes-ba1-simon/blob/master/analyse/Quizlet%20Analyse.pdf"
+        }, {
+          title: "Quizlet 2 (final)",
+          url: "https://github.com/simon-epfl/notes-ba1-simon/blob/master/analyse/Quizlet%20Analyse%20VF.pdf"
+        }]
+      },
+      {
+        shiny: true,
+        title: "Intro to programming",
+        code: "CS-107",
+        url: "https://edu.epfl.ch/coursebook/en/introduction-to-programming-CS-107",
+        buttons: [{
+          title: "Cheat Sheet (final)",
+          url: "https://github.com/simon-epfl/notes-ba1-simon/blob/master/programmation/Formulaire%20Programmation.pdf"
+        }]
+      },
+      {
+        title: "ICC",
+        code: "CS-119 (d)",
+        url: "https://edu.epfl.ch/coursebook/en/information-computation-communication-CS-119-D",
+        buttons: [],
+        shiny: true
+      }
+    ]
+  }
+]
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="grid md:grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] container mx-auto p-3">
+      <div>
+        <h1 className="text-4xl mb-4">academics.simonlefort.ch</h1>
+        <h2 className="text-1xl mb-2">Welcome!</h2>
+        <div className="flex gap-1">
+          {Object.keys(codes).map(code => (
+            // @ts-expect-error  colorMapping keyof
+            <Badge key={code} variant="outline" style={{ backgroundColor: colorMapping[code] }}>{codes[code]}</Badge>
+          ))}
         </div>
+      </div>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        {semesters.map(semester => (
+          <div key={semester.title} className="flex flex-col gap-4 w-full">
+            <h1 className="text-2xl">{semester.title}</h1>
+            <div className="grid gap-4 md:grid-cols-3 grid-cols-1">
+              {semester.courses.map(course => (
+                // @ts-expect-error colorMapping keyof
+                <Card key={course.title} className={`w-full ${course.shiny ? 'shiny-border' : ''}`} style={{ backgroundColor: colorMapping[course.code.split('-')[0]] || colorMapping.default }}>
+                  <CardHeader>
+                    <CardTitle>{course.title} - <a target="_blank" className="text-blue-600 dark:text-blue-500 hover:underline" href={course.url}>{course.code}</a></CardTitle>
+                    {course.shiny && <Badge variant="outline" className="sa-badge">SA</Badge>}
+                  </CardHeader>
+                  <CardContent className="flex gap-1">
+                    {course.buttons
+                      .slice(0, 2)
+                      .map(button => (
+                        <a href={button.url} key={button.title} target="_blank">
+                          <Button variant="outline">{button.title}</Button>
+                        </a>
+                      ))}{
+                      course.buttons.length > 2 && (
+                        <Popover>
+                          <PopoverTrigger>
+                            <Button variant="outline">+{course.buttons.length - 2}</Button>
+                          </PopoverTrigger>
+                          <PopoverContent>
+                            <div className="flex flex-col">
+                              {course.buttons
+                                .map(button => (
+                                  <a className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href={button.url} key={button.title} target="_blank">
+                                    {button.title}
+                                  </a>
+                                ))}
+                            </div>
+                          </PopoverContent>
+                        </Popover>
+                      )
+                    }
+                  </CardContent>
+                </Card>
+              ))}
+            </div>
+          </div>
+        ))}
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/simon-epfl"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -63,37 +354,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          Sources
         </a>
       </footer>
     </div>
